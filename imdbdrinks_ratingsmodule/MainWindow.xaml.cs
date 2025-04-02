b3a336ec683a1206b622b77243a40f1e4d1f1f23
915-Nicola-Razvan-Cristian <146840683+915-Nicola-Razvan-Cristian@users.noreply.github.com>
2025-04-02 20:28:49 +0300

    Database integration (#7)
    * basically works
    
    * basically works
    
    * Fixed repo + some main window stuff
    
    * all good
    
    * README updated
    
    * Updated README
    
    * fix rating
    
    * fix
    
    * Update README.md
    
    * 1
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Jonathan Mogovan <77002214+jonathan2667@users.noreply.github.com>

--- imdbdrinks_ratingsmodule/MainWindow.xaml.cs
+++ imdbdrinks_ratingsmodule/MainWindow.xaml.cs
@@ -2,7 +2,8 @@ using Microsoft.UI.Xaml;
 using imdbdrinks_ratingsmodule.Repositories;
 using imdbdrinks_ratingsmodule.Services;
 using imdbdrinks_ratingsmodule.ViewModels;
-using imdbdrinks_ratingsmodule.Domain; 
+using imdbdrinks_ratingsmodule.Domain;
+using Microsoft.UI.Xaml.Controls;
 namespace imdbdrinks_ratingsmodule
 {
 
@@ -17,9 +18,16 @@ namespace imdbdrinks_ratingsmodule
         {
             this.InitializeComponent();
 
+            // Unique connection string for MySql database (change accordingly)
+            string connection = "server=localhost;database=imdb;user=root;password=1234;";
+
+            var reviewRepo = new DatabaseReviewRepository(connection);
+            var ratingRepo = new DatabaseRatingRepository(connection);
+
+
             // Create repository instances.
-            var ratingRepo = new HardCodedRatingRepository();
-            var reviewRepo = new HardCodedReviewRepository();
+            //var ratingRepo = new HardCodedRatingRepository();
+            //var reviewRepo = new HardCodedReviewRepository();
 
             // Create service instances.
             var ratingService = new RatingService(ratingRepo);
@@ -31,26 +39,52 @@ namespace imdbdrinks_ratingsmodule
 
             // Load ratings for product ID 100.
             ViewModel.LoadRatingsForProduct(100);
-            if (ViewModel.Ratings.Count > 0)
-            {
-                ViewModel.SelectedRating = ViewModel.Ratings[0];
-                ReviewVM.LoadReviewsForRating(ViewModel.SelectedRating.RatingId);
-            }
+            //if (ViewModel.Ratings.Count > 0)
+            //{
+            //    ViewModel.SelectedRating = ViewModel.Ratings[0];
+            //    ReviewVM.LoadReviewsForRating(ViewModel.SelectedRating.RatingId);
+            //}
         }
 
         private void AddReview_Click(object sender, RoutedEventArgs e)
         {
-            var reviewWindow = new ReviewWindow(ViewModel, ReviewVM);
-            reviewWindow.Activate();
+            if (ViewModel.SelectedRating != null)
+            {
+                var reviewWindow = new ReviewWindow(ViewModel, ReviewVM);
+                reviewWindow.Activate();
+            }
+            else
+            {
+                NoRatingSelectedDialog.ShowAsync();
+            }
+            return;
 
         }
 
         private void AddRating_Click(object sender, RoutedEventArgs e)
         {
 
-           var ratingWindow = new RatingWindow(ViewModel);
+            var ratingWindow = new RatingWindow(ViewModel);
+            ViewModel.SelectedRating = null;
             ratingWindow.Activate();
         }
-       
+     
+        private void RatingSelection_Changed(object sender, RoutedEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            if (listView != null)
+            {
+                var selectedIndex = listView.SelectedIndex;
+
+                if (selectedIndex >= 0)
+                {
+                    var selectedRating = ViewModel.Ratings[selectedIndex];
+                    ViewModel.SelectedRating = selectedRating;
+                    ReviewVM.LoadReviewsForRating(selectedRating.RatingId);
+                }
+            }
+        }
+
     }
 }
