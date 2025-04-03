840c456bed27fb26dd7f277118698829bfeeba4a
915-Nicola-Razvan-Cristian <razvannicola451@gmail.com>
2025-04-03 16:10:07 +0300

    added auto close on submit

--- imdbdrinks_ratingsmodule/ReviewWindow.xaml.cs
+++ imdbdrinks_ratingsmodule/ReviewWindow.xaml.cs
@@ -69,6 +69,7 @@ namespace imdbdrinks_ratingsmodule
 
                 // Clear the TextBox.
                 ReviewTextBox.Text = string.Empty;
+                this.Close();
             }
         }
 
